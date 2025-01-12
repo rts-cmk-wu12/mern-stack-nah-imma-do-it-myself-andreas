e3866162e968ff04fd1711e06b3319ca9685f12e
BingbingDawg <u87960@rts-365.dk>
2025-01-12 21:20:19 +0100

    Merge branch 'main' of https://github.com/rts-cmk-wu12/mern-stack-nah-imma-do-it-myself-andreas

--- src/components/button.jsx
+++
@@ -1,9 +0,0 @@
-export default function Button( {children:text, show, setShow} ) {
-     function handleClick(event) {
-        setShow(!show)
-    } 
-
-    return (
-         <button onClick={handleClick}>{text}</button> 
-    )
-}
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import pagesPlugin from 'vite-plugin-pages'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), pagesPlugin()],
-})
