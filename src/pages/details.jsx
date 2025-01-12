e7adb9a994ac0865cd3d8af6221ffca162e59d38
BingbingDawg <u87960@rts-365.dk>
2025-01-12 20:36:51 +0100

    made a blogpost site that you can make and delete posts that you've made

---
+++ src/pages/details.jsx
@@ -0,0 +1,10 @@
+import PrimaryMenu from "../components/primary-menu"
+
+export default function Forside() {
+    return (
+        <>
+            <PrimaryMenu />
+            <h1>Details</h1>
+        </>
+    )
+}
